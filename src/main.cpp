6291f9a5111abc69880193915528ae8f8acd28bb
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-03-12 18:21:47 +0100

    resolved conflict

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,54 @@
 #include <iostream>
+#include <frode/ListTool2b.h>
 
-namespace gruppe32::DB  {}
+namespace gruppe32::DB  
+{
+using size_t = std::size_t;
+
+class Spillerne 
+{
+    size_t count;
+public:
+    frode::List data{ frode::Sorted };
+};
+
+
+class Idrettene 
+{
+    size_t count;
+public:
+    frode::List data { frode::Sorted };
+};
+
+struct Context 
+{
+    Idrettene idrettene;
+    Spillerne spillerne;
+};
+
+
+} // end namespace 
+
 namespace gruppe32::IO  {}
-namespace gruppe32::App {}
+namespace gruppe32::App 
+{
+
+void run(DB::Context ctx) 
+{
+    std::cout << "App running \n";
+}
+
+} // end namespace
 
 
-int main() {
+int main(int argc, char* argv[]) 
+{
     using namespace gruppe32;
-	std::cout << "HALLEO";
-	std::cin.get();
+
+    auto ctx = DB::Context{
+        .idrettene = DB::Idrettene{},
+        .spillerne = DB::Spillerne{}
+    };
+    App::run(ctx);
     return 0;
 }
