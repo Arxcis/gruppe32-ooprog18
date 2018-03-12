4c486e4d4e4b839a52f7da4e2251c47dd3d9094d
Halvor Smedås <stektpotet@gmail.com>
2018-03-12 16:55:31 +0100

    Updated CMakeLists.txt to work with Windows and VisualStudio

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
 
 namespace gruppe32::DB  {}
 namespace gruppe32::IO  {}
 namespace gruppe32::App {}
 
+
 int main() {
     using namespace gruppe32;
-
+	std::cout << "HALLEO";
+	std::cin.get();
     return 0;
 }
