abdcbcba05b8ee410707dafa93ea1e477283f85a
Halvor Smedås <stektpotet@gmail.com>
2018-03-13 17:03:46 +0100

    DB.hpp - initial definition of datatypes

--- include/gruppe32/DB.hpp
+++ include/gruppe32/DB.hpp
@@ -1,19 +1,83 @@
 #pragma once
 #include <cstddef>
+#include <string>
+#include <vector>
+#include <array>
 #include <frode/ListTool2b.h>
 
 namespace gruppe32::DB  
 {
-using size_t = std::size_t;
+//Using directives:
+using std::size_t;
+using std::vector;
+using std::array;
 
-class Spillerne 
+/**
+ *
+ */
+class Spiller 
+{
+	size_t id;
+	string name;
+	string address;
+public:
+
+};
+
+
+class Spillerne
 {
     size_t count;
 public:
     frode::List data{ frode::Sorted };
 };
 
+class Resultat
+{
+	const size_t LENGTH_DATE = 9;
+	const size_t MAX_SPILLERE = 50;
 
+	array<char, DATE_LENGTH> dato;
+	bool overtid;
+	array<size_t, MAX_SPILLERE> hjemmeScorere;
+	array<size_t, MAX_SPILLERE> borteScorere;
+private:
+	/*
+
+	size_t hjemmeMål, borteMål;
+	*/
+};
+
+class Divisjon
+{
+	const size_t MAX_LAG = 30;
+
+	string navn;
+	array<Lag, MAX_LAG> lag;
+	array<vector<Resultat>, MAX_LAG> terminListe;
+public:
+	/*
+	size_t lagCount() {
+		return lag.size();
+	}
+	*/
+};
+
+class Idrett
+{
+	enum Tabell
+	{
+		SEIER_2_UAVGJORT_1_TAP_0,
+		SEIER_3_UAVGJORT_1_TAP_0,
+		SEIER_3_OVERTID_2_UAVGJORT_1_TAP_0
+	};
+
+	vector<Divisjon> divisjoner;
+	Tabell tabell;
+	string name;
+public:
+
+};
 class Idrettene 
 {
     size_t count;
