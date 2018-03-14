2539a9364dbb5124220150443863b06bf498a2af
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-03-14 17:29:07 +0100

    DB.hpp - moved usings on top of every class to make the code clearer

--- include/gruppe32/DB.hpp
+++ include/gruppe32/DB.hpp
@@ -9,95 +9,112 @@
 /// <summary> Database definitions </summary>
 namespace gruppe32::DB  
 {
-//Using directives:
-using std::size_t;
-using std::vector;
-using std::array;
-using std::string;
 
-/**
- *
- */
+
+using std::string;
+using std::size_t;
+
 class Spiller 
 {
-	size_t id;
-	string name;
-	string address;
 public:
+    size_t id;
+    string name;
+    string address;
 
 };
 
 
+using std::size_t;
+
 class Spillerne
 {
-    size_t count;
+
 public:
+    std::size_t count;
     listtool::List data{ listtool::Sorted };
 };
 
 
+using std::size_t;
+using std::array;
+using std::string;
+
 class Lag 
 {
 public:
-	constexpr static size_t MAX_SPILLERE = 50;
-private:
-	string navn;
-	string adresse;
-	array<size_t, MAX_SPILLERE> spillere;
+    constexpr static size_t MAX_SPILLERE = 50;
+    string navn;
+    string adresse;
+    array<size_t, MAX_SPILLERE> spillere;
 };
 
+
+using std::size_t;
+using std::array;
 
 class Resultat
-{
-	constexpr static size_t LENGTH_DATE = 9;
-
-	array<char, LENGTH_DATE> dato;
-	bool overtid;
-	array<size_t, Lag::MAX_SPILLERE> hjemmeScorere;
-	array<size_t, Lag::MAX_SPILLERE> borteScorere;
+{    
 public:
-	/*
-
-	size_t hjemmeMål, borteMål;
-	*/
+    constexpr static size_t LENGTH_DATE = 9;
+    array<char, LENGTH_DATE> dato;
+    bool overtid;
+    array<size_t, Lag::MAX_SPILLERE> hjemmeScorere;
+    array<size_t, Lag::MAX_SPILLERE> borteScorere;
+    /*
+    size_t hjemmeMål, borteMål;
+    */
 };
 
 
+using std::size_t;
+using std::string;
+using std::array;
+using std::vector;
 
 class Divisjon
 {
-	constexpr static size_t MAX_LAG = 30;
+private:
+    constexpr static size_t MAX_LAG = 30;
 
-	string navn;
-	array<Lag, MAX_LAG> lag;
-	array<vector<Resultat>, MAX_LAG> terminListe;
+    string navn;
+    array<Lag, MAX_LAG> lag;
+    array<vector<Resultat>, MAX_LAG> terminListe;
 public:
-	/*
-	size_t lagCount() {
-		return lag.size();
-	}
-	*/
+    /*
+    size_t lagCount() {
+        return lag.size();
+    }
+    */
 };
+
+
+using std::string;
+using std::vector;
 
 class Idrett
 {
-	enum Tabell
-	{
-		SEIER_2_UAVGJORT_1_TAP_0,
-		SEIER_3_UAVGJORT_1_TAP_0,
-		SEIER_3_OVERTID_2_UAVGJORT_1_TAP_0
-	};
-
-	vector<Divisjon> divisjoner;
-	Tabell tabell;
-	string name;
 public:
+    enum Tabell
+    {
+        SEIER_2_UAVGJORT_1_TAP_0,
+        SEIER_3_UAVGJORT_1_TAP_0,
+        SEIER_3_OVERTID_2_UAVGJORT_1_TAP_0
+    };
+
+private:
+    vector<Divisjon> divisjoner;
+    Tabell tabell;
+    string name;
 
 };
+
+
+using std::size_t;
+
 class Idrettene 
 {
-    size_t count;
 public:
+    size_t count;
     listtool::List data { listtool::Sorted };
 };
 
