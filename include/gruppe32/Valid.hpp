ae43ccce6f1749b00f50af88b72db11109d913ec
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-03-14 15:26:29 +0100

    VAlid.cpp/hpp - hiding implementation details of, moving stuff frop hpp -> cpp file

--- include/gruppe32/Valid.hpp
+++ include/gruppe32/Valid.hpp
@@ -10,55 +10,38 @@
 /// <summary> String validation module </summary>
 namespace gruppe32::Valid 
 {
-    /// <summary> string - validating single letter A-Z, a-z </summary>
-    constexpr char regexAsciiChar[] = R"(^[a-zA-Z]$)";
-    
-    /// <summary> string - validating single letter A-Z, a-z, æ,ø,å,Æ,Ø,Å </summary>
-    constexpr char regexAsciiCharPlusNorwegian[] = R"(^[a-zA-ZøæåØÆÅ]$)";
-    
-    /// <summary> string - validating any norwegian postnumber between 0001-9999 </summary>
-    constexpr char regexPostnummer[] = R"(^000[1-9]$|^00[1-9][0-9]$|^0[1-9][0-9]{2}$|^[1-9][0-9]{3}$)";
+using std::string;
 
-    /// <summary> string - validating uint Min 0, Max 3 999 999 999 </summary>
-    constexpr char regexUint[] = R"((^[1-9][0-9]{0,8}$)|(^[1-3]([0-9]{0,9})$|(^0$)))";
+/// <summary> string - validating single letter A-Z, a-z </summary>
+bool isAsciiChar(const string& value);
 
-    ///  <summary> string - validating any norwegian single name, with an optional hyphen to
-    ///                     to separate a composite name.
-    ///                     Example of valid names:
-    ///                     * Jonas
-    ///                     * NORGE
-    ///                     * NorGe
-    ///                     * Bjørn-Magne </summary>
-    constexpr char regexName[] = R"(^([a-zA-ZÆØÅæøå]+)(-[a-zA-ZÆØÅæøå]+)?$)";
-    
-    /// <summary> string - validating a year 'yyyy' in the range [1970-2099] </summary>
-    constexpr char regexYear[] = R"(^19[7-9][0-9]$|^20[0-9][0-9]$)";
+/// <summary> string - validating single letter A-Z, a-z, æ,ø,å,Æ,Ø,Å </summary>
+bool isAsciiCharPlusNorwegian(const string& value);
 
-    /// <summary> string - validating a month 'mm' in the range [01-12] </summary>
-    constexpr char regexMonth[] = R"(^0[1-9]$|^1[012]$)";
+/// <summary> string - validating any norwegian postnumber between 0001-9999 </summary>
+bool isPostnummer(const string& value);
 
-    /// <summary> string - validating a day 'dd' in the range [01-31] </summary>
-    constexpr char regexDay[] = R"(^0[1-9]$|^[12][0-9]$|^3[01]$)";
+/// <summary> string - validating uint Min 0, Max 3 999 999 999 </summary>
+bool isUint(const string& value);
 
-    /// <summary> string - validating a norwegian address. robustness is very-relaxed </summary>
-    constexpr char regexAddress[]  = R"(^[A-Za-z0-9\ \. \-ÆØÅæøå]+$)";
+///  <summary> string - validating any norwegian single name, with an optional hyphen to
+///                     to separate a composite name.
+///                     Example of valid names:
+///                     * Jonas
+///                     * NORGE
+///                     * NorGe
+///                     * Bjørn-Magne </summary>
+bool isName(const string& value);
 
-    using std::string;
+/// <summary> string - validating a year 'yyyy' in the range [1970-2099] </summary>
+bool isYear(const string& value);
 
-    /// <summary> isAsciiChar() </summary>
-    bool isAsciiChar(const string& value);
-    /// <summary> isPostnummer() </summary>
-    bool isPostnummer(const string& value);
-    /// <summary> isUint() </summary>
-    bool isUint(const string& value);
-    /// <summary> isName() </summary>
-    bool isName(const string& value);
-    /// <summary> isYear() </summary>
-    bool isYear(const string& value);
-    /// <summary> isMonth() </summary>
-    bool isMonth(const string& value);
-    /// <summary> isDay() </summary>
-    bool isDay(const string& value);
-    /// <summary> isAddress() </summary>
-    bool isAddress(const string& value);
+/// <summary> string - validating a month 'mm' in the range [01-12] </summary>
+bool isMonth(const string& value);
+
+/// <summary> string - validating a day 'dd' in the range [01-31] </summary>
+bool isDay(const string& value);
+
+/// <summary> string - validating a norwegian address. robustness is very-relaxed </summary>
+bool isAddress(const string& value);
 }
--- src/Valid.cpp
+++ src/Valid.cpp
@@ -2,48 +2,74 @@
 
 namespace gruppe32::Valid
 {
-    using std::string;
-    using std::regex;
-    using std::regex_match;
 
-    bool isAsciiChar(const string& value)
-    {
-        const static regex compiledRegex(Valid::regexAsciiChar);
-        return regex_match(value, compiledRegex);
-    }
-    bool isPostnummer(const string& value)
-    {   
-        const static regex compiledRegex(Valid::regexPostnummer);
-        return regex_match(value, compiledRegex);
-    }
-    bool isUint(const string& value)
-    {
-        const static regex compiledRegex(Valid::regexUint);
-        return regex_match(value, compiledRegex);
-    }
-    bool isName(const string& value)
-    {
-        const static regex compiledRegex(Valid::regexName);
-        return regex_match(value, compiledRegex);
-    }
-    bool isYear(const string& value)
-    {
-        const static regex compiledRegex(Valid::regexYear);
-        return regex_match(value, compiledRegex);
-    }
-    bool isMonth(const string& value)
-    {
-        const static regex compiledRegex(Valid::regexMonth);
-        return regex_match(value, compiledRegex);
-    }
-    bool isDay(const string& value)
-    {
-        const static regex compiledRegex(Valid::regexDay);
-        return regex_match(value, compiledRegex);
-    } 
-    bool isAddress(const string& value)
-    {
-        const static regex compiledRegex(Valid::regexAddress);
-        return regex_match(value, compiledRegex);
-    }   
+using std::string;
+using std::regex;
+using std::regex_match;
+
+bool isAsciiChar(const string& value)
+{
+    constexpr char regexAsciiChar[] = R"(^[a-zA-Z]$)";
+
+    const static regex compiledRegex(regexAsciiChar);
+    return regex_match(value, compiledRegex);
+}
+
+bool isAsciiCharPlusNorwegian(const string& value)
+{
+    constexpr char regexAsciiCharPlusNorwegian[] = R"(^[a-zA-ZøæåØÆÅ]$)";
+
+    const static regex compiledRegex(regexAsciiCharPlusNorwegian);
+    return regex_match(value, compiledRegex);   
+}
+
+bool isPostnummer(const string& value)
+{   
+    constexpr char regexPostnummer[] = R"(^000[1-9]$|^00[1-9][0-9]$|^0[1-9][0-9]{2}$|^[1-9][0-9]{3}$)";
+
+    const static regex compiledRegex(regexPostnummer);
+    return regex_match(value, compiledRegex);
+}
+bool isUint(const string& value)
+{
+    constexpr char regexUint[] = R"((^[1-9][0-9]{0,8}$)|(^[1-3]([0-9]{0,9})$|(^0$)))";
+
+    const static regex compiledRegex(regexUint);
+    return regex_match(value, compiledRegex);
+}
+bool isName(const string& value)
+{
+    constexpr char regexName[] = R"(^([a-zA-ZÆØÅæøå]+)(-[a-zA-ZÆØÅæøå]+)?$)";
+
+    const static regex compiledRegex(regexName);
+    return regex_match(value, compiledRegex);
+}
+bool isYear(const string& value)
+{
+    constexpr char regexYear[] = R"(^19[7-9][0-9]$|^20[0-9][0-9]$)";
+
+    const static regex compiledRegex(regexYear);
+    return regex_match(value, compiledRegex);
+}
+bool isMonth(const string& value)
+{
+    constexpr char regexMonth[] = R"(^0[1-9]$|^1[012]$)";
+
+    const static regex compiledRegex(regexMonth);
+    return regex_match(value, compiledRegex);
+}
+bool isDay(const string& value)
+{
+    constexpr char regexDay[] = R"(^0[1-9]$|^[12][0-9]$|^3[01]$)";
+
+    const static regex compiledRegex(regexDay);
+    return regex_match(value, compiledRegex);
+} 
+bool isAddress(const string& value)
+{
+    constexpr char regexAddress[]  = R"(^[A-Za-z0-9\ \. \-ÆØÅæøå]+$)";
+
+    const static regex compiledRegex(regexAddress);
+    return regex_match(value, compiledRegex);
+}   
 } // end namespace
