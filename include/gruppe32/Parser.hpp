fe89e1e38c11bbd5c83cc33ab5711f2670cacbb4
Jonas Johan Solsvik <jonas.solsvik@gmail.com>
2018-03-16 00:18:40 +0100

    Parser.hpp, Parse.cpp - new module
    * Created declarations.
    * Partially documented all functions.

---
+++ include/gruppe32/Parser.hpp
@@ -0,0 +1,83 @@
+#pragma once
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <string_view>
+
+#include <gruppe32/DB.hpp>
+
+/// <summary> Parsing strings to internal datastructure and vice versa </summary>
+namespace gruppe32::Parser
+{
+
+
+using std::size_t;
+using std::string;
+using std::string_view;
+using DB::Idrettene;
+using DB::Spillerene
+using Status = std::size_t;
+
+constexpr Parse::Status SUCCESS = 0;
+constexpr Parse::Status ERROR = 1;
+
+
+/// <summary> Encode. Has a corresponding decode function. </summary>
+/// <returns> string - example found in format-idrettene.yml </returns>
+auto encodeIdrettene(const DB::Idrettene& idrettene) -> string;
+
+/// <summary> Encode. Has a corresponding decode function. </summary>
+/// <returns> string - example found in format-spillerene.yml </returns>
+auto encodeSpillerene(const DB::Spillerene& spillerene) -> string;
+
+/// <summary> Decode. Has a corresponding encode function. </summary>
+/// <param name="strview"> example found in format-idrettene.yml </param name="strview">
+auto decodeIdrettene(DB::Idrettene& idrettene, string_view strview) -> Parse::Status;
+
+/// <summary> Decode. Has a corresponding encode function. </summary>
+/// <param name="strview"> example found in format-spillerene.yml </param name="strview">
+auto decodeSpillerene(DB::Spillerene& spillerene, string_view strview) -> Parse::Status;
+
+
+
+/// <summary> Encode-only. </summary>
+/// <returns> string - example found in format-resultatene-divisjon.yml </returns>
+auto encodeResultateneDivisjon(const DB::Divisjon& divisjon) -> string;
+
+/// <summary> Encode-only.</summary>
+/// <returns> string - example found in format-resultatene-idrett.yml </returns>
+auto encodeResultateneIdrett(const DB::Idrett& idrett) -> string;
+
+/// <summary> Encode-only. </summary>
+/// <returns> string - example found in format-tabell-divisjon.yml </returns>
+auto encodeTabellDivisjon(const DB::Divisjon& divisjon) -> string;
+
+/// <summary> Encode-only. Output example:  </summary>
+/// <returns> string - example found in format-tabellene-idrett.yml </returns>
+auto encodeTabelleneIdrett(const DB::Idrett& idrett) -> string;
+
+/// <summary> Encode-only. Output example:  </summary>
+/// <returns> string - example found in format-terminliste-divisjon.yml </returns>
+auto encodeTerminlisteDivisjon(const DB::Divisjon& divisjon) -> string;
+
+/// <summary> Encode-only. Output example:  </summary>
+/// <returns> string - example found in format-toppscorerene-divisjon.yml </returns>
+auto encodeToppscorereneDivisjon(const DB::Divisjon& divisjon) -> string;
+
+/// <summary> Encode-only. Output example:  </summary>
+/// <returns> string - example found in format-toppscorerene-lag.yml </returns>
+auto encodeToppscorereneLag(const DB::Divisjon& divisjon, const string lagnavn) -> string;
+
+
+
+/// <summary> Decode-only. </summary>
+/// <param name="strview"> example found in format-resultatene.yml </param name="strview">
+auto decodeResultatene(DB::Idrettene& idrettene, string_view strview) -> Parse::Status;
+
+/// <summary> Decode-only. </summary>
+/// <param name="strview"> example found in format-divisjon.yml </param name="strview">
+auto decodeDivisjon(DB::Divisjon& divisjon, string_view strview) -> Parse::status; 
+
+
+} // end namespace Parse
---
+++ src/Parser.cpp
@@ -0,0 +1,6 @@
+#include <gruppe32/Parse.hpp>
+
+namespace gruppe32::Parser 
+{
+    
+}
